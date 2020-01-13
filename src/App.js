90483a0e610751b0cbec6035cc1a083d7b31f057
Kun Yu <kun.yu25@myhunter.cuny.edu>
2020-01-13 00:23:19 -0500

    Moved dummy data into store

--- src/App.js
+++ src/App.js
@@ -4,60 +4,60 @@ import { connect } from "react-redux";
 import { addStudent } from "./store/utilities/student";
 
 //Dummy data
-let student1 = {
-  id: 2,
-  firstName: "bob",
-  lastName: "jones",
-  email: "bobbyboy1234@yahoo.com",
-  gpa: 3.7,
-  campusId: 1
+let dummyStudent = {
+	id: 5,
+	firstName: "bob",
+	lastName: "jones",
+	email: "bobbyboy1234@yahoo.com",
+	gpa: 3.7,
+	campusId: 1
 };
 
 class App extends Component {
-  addStudent = student => {
-    this.props.addStudent(student);
-  };
+	addStudent = student => {
+		this.props.addStudent(student);
+	};
 
-  onSubmit(student1) {
-    this.props.addStudent(student1);
-  }
+	onSubmit(student) {
+		this.props.addStudent(student);
+	}
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Hello World</h1>
-          <button type="button" onClick={() => this.onSubmit(student1)}>
-            Submit
-          </button>
-          {console.log(this.props.allStudents)}
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1>Hello World</h1>
+					<button type="button" onClick={() => this.onSubmit(dummyStudent)}>
+						Submit
+					</button>
+					{console.log(this.props.allStudents)}
+				</header>
+			</div>
+		);
+	}
 }
 
 // Declaration for mapStateToProps;
 // The keys in this returned object will be on your component's `props` object;
 // The values of these keys reflect the value of the piece of state in your Redux store;
 const mapState = state => {
-  return {
-    allStudents: state.studentReducer
-  };
+	return {
+		allStudents: state.studentReducer
+	};
 };
 
 // Declaration for mapDispatchToProps;
 // The keys in this returned object will be on your component's `props` object as well;
 // The values of these keys are anonymous functions that will dispatch imported action creators or thunks so that a component can communicate with the appropriate reducer function(s);
 const mapDispatch = dispatch => {
-  return {
-    //incrementCounter: (amount) => dispatch(increment(amount)),
-    //decrementCounter: (amount) => dispatch(decrement(amount)),
-    //fetchAllStudents: () => dispatch(fetchStudentsThunk()),
-    //removeStudent: (id) => dispatch(removeStudentThunk(id)),
-    //addStudent: student => dispatch(addStudentThunk(student))
-    addStudent: student => dispatch(addStudent(student))
-  };
+	return {
+		//incrementCounter: (amount) => dispatch(increment(amount)),
+		//decrementCounter: (amount) => dispatch(decrement(amount)),
+		//fetchAllStudents: () => dispatch(fetchStudentsThunk()),
+		//removeStudent: (id) => dispatch(removeStudentThunk(id)),
+		//addStudent: student => dispatch(addStudentThunk(student))
+		addStudent: student => dispatch(addStudent(student))
+	};
 };
 
 export default connect(mapState, mapDispatch)(App);
--- src/store/utilities/student.js
+++ src/store/utilities/student.js
@@ -2,10 +2,45 @@
 const ADD_STUDENT = "ADD_STUDENT";
 const SHOW_STUDENT = "SHOW_STUDENT";
 
+const dummyStudents = [
+	{
+		id: 1,
+		firstName: "bob",
+		lastName: "jones",
+		email: "bobbyboy1234@yahoo.com",
+		gpa: 3.7,
+		campusId: 1
+	},
+	{
+		id: 2,
+		firstName: "bob2",
+		lastName: "jones",
+		email: "bobbyboy1234@yahoo.com",
+		gpa: 3.7,
+		campusId: 1
+	},
+	{
+		id: 3,
+		firstName: "bob4",
+		lastName: "jones",
+		email: "bobbyboy1234@yahoo.com",
+		gpa: 3.7,
+		campusId: 2
+	},
+	{
+		id: 4,
+		firstName: "bob4",
+		lastName: "jones",
+		email: "bobbyboy1234@yahoo.com",
+		gpa: 3.7,
+		campusId: 2
+	}
+];
+
 //INITIAL STATE
 //allStudents and allCampuses will start off as empty
 const initialState = {
-  allStudents: []
+	allStudents: dummyStudents
 };
 
 //ACTION CREATERS
@@ -13,40 +48,40 @@ const initialState = {
 //Used for ADD_STUDENT
 //No payload b/c we are only using this to add the student to the array of allStudents
 export const addStudent = student => {
-  return {
-    /*
+	return {
+		/*
     type: ADD_STUDENT,
     allStudents: [...state.allStudents, student]
     */
-    type: ADD_STUDENT,
-    payload: student
-  };
+		type: ADD_STUDENT,
+		payload: student
+	};
 };
 
 //Used for SHOW_STUDENT action type -- will return payload later on
 //student becomes the payload -- payload = data about student!
 export const fetchStudent = student => {
-  return {
-    type: SHOW_STUDENT,
-    payload: student
-  };
+	return {
+		type: SHOW_STUDENT,
+		payload: student
+	};
 };
 
 //THUNKS, SKIP FOR NOW CAUSE WE DONT HAVE ANY ASYNCH STUFF YET LOL
 
 //REDUCER FUNCTIONS
 function studentReducer(state = initialState, action) {
-  switch (action.type) {
-    case SHOW_STUDENT:
-      return action.payload;
-    case ADD_STUDENT:
-      return {
-        ...state,
-        allStudents: [...state.allStudents, action.payload]
-      };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case SHOW_STUDENT:
+			return action.payload;
+		case ADD_STUDENT:
+			return {
+				...state,
+				allStudents: [...state.allStudents, action.payload]
+			};
+		default:
+			return state;
+	}
 }
 
 export default studentReducer;
