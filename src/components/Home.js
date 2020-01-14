aa3d2b6ff3e87a6c8997e36b2921c10b241acce6
Kun Yu <kun.yu25@myhunter.cuny.edu>
2020-01-14 15:11:00 -0500

    make background image only apply to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./Home.css";
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  );
+	return (
+		<div className="main-container">
+			<h1>Home Page</h1>
+		</div>
+	);
 };
 
 export default Home;
