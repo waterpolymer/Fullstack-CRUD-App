35cb0801a473c5249aad3f39ae58465bf7523ec8
Sami Beig <Sami.Beig45@myhunter.cuny.edu>
2020-01-14 11:27:19 -0500

    added background image to all pages

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
 import React, { Component } from "react";
 import {
-	Home,
-	AllStudents,
-	AddStudent,
-	SingleStudent,
-	AllCampuses
+  Home,
+  AllStudents,
+  AddStudent,
+  SingleStudent,
+  AllCampuses
 } from "./components";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavBar from './components/NavBar';
+import NavBar from "./components/NavBar";
 
 class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<Router basename="">
-					<NavBar />
-					<div>
-						<Route exact path="/" render={() => <Home />} />
-						<Route exact path="/students" render={() => <AllStudents />} />
-						<Route exact path="/campuses" render={() => <AllCampuses />} />
-						<Route
-							exact
-							path="/students/:studentid"
-							render={props => <SingleStudent {...props} />}
-						/>
-						<Route
-							exact
-							path="/students/add-student"
-							render={() => <AddStudent />}
-						/>
-					</div>
-				</Router>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="App">
+        <Router basename="">
+          <NavBar />
+          <div>
+            <Route exact path="/" render={() => <Home />} />
+            <Route exact path="/students" render={() => <AllStudents />} />
+            <Route exact path="/campuses" render={() => <AllCampuses />} />
+            <Route
+              exact
+              path="/students/:studentid"
+              render={props => <SingleStudent {...props} />}
+            />
+            <Route
+              exact
+              path="/students/add-student"
+              render={() => <AddStudent />}
+            />
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import LinkButton from "./LinkButton";
 
 const Home = props => {
-	return (
-		<div>
-			<h1>Hello World</h1>
-			<LinkButton to="/students">All Students</LinkButton>
-			<LinkButton to="/campuses">All Campuses</LinkButton>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <LinkButton to="/students">All Students</LinkButton>
+      <LinkButton to="/campuses">All Campuses</LinkButton>
+    </div>
+  );
 };
 
 export default Home;
