191ef211d1318bc746e549af289ccdb7145bd2ac
Sami Beig <Sami.Beig45@myhunter.cuny.edu>
2020-01-16 15:30:13 -0500

    changed name on navbar to School Manager

--- src/components/utilities/NavBar.js
+++ src/components/utilities/NavBar.js
@@ -3,25 +3,25 @@ import { Link } from "react-router-dom";
 import "./NavBar.css";
 
 const NavBar = () => {
-	return (
-		<ul>
-			<li className="left">
-				<Link className="logo" to="/">
-					CRUD
-				</Link>
-			</li>
-			<li>
-				<Link className="item" to="/campuses">
-					Campuses
-				</Link>
-			</li>
-			<li>
-				<Link className="item" to="/students">
-					Students
-				</Link>
-			</li>
-		</ul>
-	);
+  return (
+    <ul>
+      <li className="left">
+        <Link className="logo" to="/">
+          School Manager
+        </Link>
+      </li>
+      <li>
+        <Link className="item" to="/campuses">
+          Campuses
+        </Link>
+      </li>
+      <li>
+        <Link className="item" to="/students">
+          Students
+        </Link>
+      </li>
+    </ul>
+  );
 };
 
 export default NavBar;
